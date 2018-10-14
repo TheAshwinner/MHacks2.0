7dead2d3cb7cd24c20c46be4e45a30c32f8e22ee
Ashwin Sreevatsa <public.ashwinsreevatsa@gmail.com>
2018-10-13 20:51:02 -0400

    Create ticket class

--- app/src/main/java/com/ashwinsreevatsa/michigantickets2/Buying.java
+++ app/src/main/java/com/ashwinsreevatsa/michigantickets2/Buying.java
@@ -3,11 +3,24 @@ package com.ashwinsreevatsa.michigantickets2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Buying extends AppCompatActivity {
+
+
+    private DatabaseReference firebaseDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buying);
+
+        //Getting reference for firebase db so it can be accessed later on
+        firebaseDb = FirebaseDatabase.getInstance().getReference();
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/ashwinsreevatsa/michigantickets2/Data/Ticket.java
@@ -0,0 +1,33 @@
+package com.ashwinsreevatsa.michigantickets2.Data;
+
+/**
+ * Ticket object along with information needed for each ticket including game, section, seller, phone
+ * number, and price
+ */
+public class Ticket {
+    private String game; //Game (Nebraska, Ohio St, etc)
+    private int seatSection;
+    private int seatRow;
+    private int seatNum; //Seat number
+
+    private String name;
+    private int phoneNumber;
+    private double price; //Asking price of the seller
+
+    public Ticket(){
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public Ticket(String game, int seatSection, int seatRow, int seatNum, String name,
+                  int phoneNumber, double price){
+        this.game = game;
+        this.seatSection = seatSection;
+        this.seatRow = seatRow;
+        this.seatNum = seatNum;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.price = price;
+
+
+    }
+}
