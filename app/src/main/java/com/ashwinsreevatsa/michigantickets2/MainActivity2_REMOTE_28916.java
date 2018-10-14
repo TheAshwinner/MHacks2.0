02477a096e9334ad68444ab4168527fcb46f835f
Ashwin Sreevatsa <public.ashwinsreevatsa@gmail.com>
2018-10-14 01:15:49 -0400

    Fix error where files are deleted... hopefully

---
+++ app/src/main/java/com/ashwinsreevatsa/michigantickets2/MainActivity2_REMOTE_28916.java
@@ -0,0 +1,52 @@
+package com.ashwinsreevatsa.michigantickets2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+private Button button;
+private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        //Define the Switching Activities Button
+        button = (Button) findViewById(R.id.sellBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSelling();
+
+
+            }
+        });
+
+        //Define the Switching Activities Button
+        button2 = (Button) findViewById(R.id.buyBtn);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBuying();
+            }
+        });
+        }
+
+        public void openSelling() {
+            Intent intent = new Intent(this, Selling.class);
+            startActivity(intent);
+
+
+        }
+
+        public void openBuying() {
+            Intent intent = new Intent(this, Buying.class);
+            startActivity(intent);
+
+        }
+    }
+
